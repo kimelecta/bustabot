bee75fb0cefec503b4d0adfdaf117eca050351fb
kimelecta <57180272+kimelecta@users.noreply.github.com>
2020-06-17 21:11:34 +0800

    Update bustabit_script.js

--- bustabit_script.js
+++ bustabit_script.js
@@ -1,44 +1,34 @@
 var config = {
-  wager: {
-    value: 100, type: 'balance', label: 'wager'
-  },
-  payout: {
-    value: 2, type: 'multiplier', label: 'payout' }
+  bet: { value: 100, type: 'balance', label: 'Initial Bet' },
+  cashOut: { value: 2, type: 'multiplier', label: 'Initial Cashout' },
 };
 
-// This is the Flat bet script from the bustabit site
-// Replace this file by your own bot
-// Script files must begin with 'var config = {}'
+var cashOut = config.cashOut.value;
 
-// Try to bet immediately when script starts
-if (engine.gameState === "GAME_STARTING") {
-  makeBet();
-}
+engine.bet(config.bet.value, cashOut);
 
 engine.on('GAME_STARTING', onGameStarted);
 engine.on('GAME_ENDED', onGameEnded);
 
+
 function onGameStarted() {
-  makeBet();
+  engine.bet(config.bet.value, cashOut);
 }
 
 function onGameEnded() {
-  var lastGame = engine.history.first();
+	var lastGame = engine.history.first()
 
-  // If we wagered, it means we played
-  if (!lastGame.wager) {
-    return;
-  }
+	if (!lastGame.wager) {
+    	return;
+  	}
 
-  if (lastGame.cashedAt) {
-    var profit = Math.round((config.wager.value * config.payout.value - config.wager.value) / 100)
-    log('we won', profit, 'bits');
-  } else {
-    log('we lost', Math.round(config.wager.value / 100), 'bits');
-  }
+  	// won
+  	if (lastGame.cashedAt) {
+    	log('Cicle ended starting a new one ...');
+    	cashOut = config.cashOut.value;
+  	} else {
+  	// lost
+  		cashOut = cashOut + config.bet.value;
+  	}
 }
 
-function makeBet() {
-  engine.bet(config.wager.value, config.payout.value);
-  log('betting', Math.round(config.wager.value / 100), 'on', config.payout.value, 'x');
-}
